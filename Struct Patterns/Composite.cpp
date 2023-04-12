8cea3c7e7197f8a6e0906b15429fd45ddca51760
gitBin <2782694792@qq.com>
2023-04-12 23:23:29 +0800

    组合模式 —— (纯)虚函数控制

--- Struct Patterns/Composite.cpp
+++ Struct Patterns/Composite.cpp
@@ -1,144 +1,94 @@
-#pragma once
-
-#include <string>
-
-class Entry
-{
-protected:
-    Entry();
-
-public:
-    // virtual void Operation() = 0; // error
-    virtual ~Entry() {}
-    virtual std::string getName() const = 0;
-    virtual int getSize() const = 0;
-    virtual void addEntry(Entry *entry) {}
-    virtual void printList(const std::string &str) = 0;
-
-    void printList()
-    {
-        printList("");
-    }
-    std::string toString() const
-    {
-        return getName() + " (" + std::to_string(getSize()) + ")";
-    }
-};
-
 #include <iostream>
 #include <vector>
 
-class Directory : public Entry
+class Component
 {
 public:
-    Directory(const std::string &name) : m_name(name) {}
-    ~Directory()
+    virtual ~Component() {}
+
+    virtual Component *getChild(const unsigned int)
     {
-        for (auto it : m_dirs)
-        {
-            delete it;
-        }
-        m_dirs.clear();
-    }
-    virtual std::string getName() const override
-    {
-        return m_name;
-    }
-    virtual int getSize() const
-    {
-        int size = 0;
-        for (auto it : m_dirs)
-        {
-            size += it->getSize();
-        }
-        return size;
-    }
-    virtual void addEntry(Entry *entry) override
-    {
-        m_dirs.push_back(entry);
-    }
-    virtual void printList(const std::string &str) override
-    {
-        std::cout << str << "/" << toString() << std::endl;
-        for (auto it : m_dirs)
-        {
-            it->printList(str + "/" + m_name);
-        }
+        return 0;
     }
 
-private:
-    std::string m_name;
-    std::vector<Entry *> m_dirs;
+    virtual void add(Component *)
+    {
+    }
+    
+    virtual void remove(const unsigned int )
+    {
+    }
+
+    virtual void operation() = 0;
 };
 
-class File : public Entry
+class Composite : public Component
 {
 public:
-    File(const std::string &name, int size) : m_name(name), m_size(size) {}
-    virtual std::string getName() const override
+    ~Composite()
     {
-        return m_name;
-    }
-    virtual int getSize() const override
-    {
-        return m_size;
+        for (unsigned int i = 0; i < children.size(); i++)
+        {
+            delete children[i];
+        }
     }
 
-    virtual void printList(const std::string &str) override
+    Component *getChild(const unsigned int index) override
     {
-        std::cout << str << "/" << toString() << std::endl;
+        return children[index];
+    }
+
+    void add(Component *component)override
+    {
+        children.push_back(component);
+    }
+
+    void remove(const unsigned int index)override
+    {
+        Component *child = children[index];
+        children.erase(children.begin() + index);
+        delete child;
+    }
+
+    void operation()override
+    {
+        for (unsigned int i = 0; i < children.size(); i++)
+        {
+            children[i]->operation();
+        }
     }
 
 private:
-    std::string m_name;
-    int m_size;
+    std::vector<Component *> children;
+};
+
+class Leaf : public Component
+{
+public:
+    Leaf(const int i) : id(i) {}
+
+    ~Leaf() {}
+
+    void operation()override
+    {
+        std::cout << "Leaf " << id << " operation" << std::endl;
+    }
+
+private:
+    int id;
 };
 
 int main()
 {
-    Directory *root = new Directory("root");
-    Directory *bin = new Directory("bin");
-    Directory *tmp = new Directory("tmp");
-    Directory *usr = new Directory("usr");
+    Composite composite;
 
-    root->addEntry(bin);
-    root->addEntry(tmp);
-    root->addEntry(usr);
-    bin->addEntry(new File("vi", 3000));
-    bin->addEntry(new File("latex", 2000));
-    static_cast<Entry *>(root)->printList();
-
-    Directory *yuki = new Directory("yuki");
-    Directory *hanako = new Directory("hanako");
-    Directory *tomura = new Directory("tomura");
-
-    usr->addEntry(yuki);
-    usr->addEntry(hanako);
-    usr->addEntry(tomura);
-    yuki->addEntry(new File("diary.html", 100));
-    hanako->addEntry(new File("memo.tex", 1024));
-    tomura->addEntry(new File("junk.mail", 40));
-
-    static_cast<Entry *>(root)->printList();
-
-    // for test coverage
+    for (unsigned int i = 0; i < 5; i++)
     {
-        File *f = new File("123.txt", 1);
-        f->addEntry(root);
-        f->printList("");
-        delete f;
+        composite.add(new Leaf(i));
     }
-    {
-        yuki->printList("");
-    }
-    {
-        File *f = new File("123.txt", 1);
-        Entry *e = new Directory("test");
-        e->addEntry(f);
-        e->printList("");
-        e->toString();
-        delete e;
-    }
-    delete root;
+
+    composite.remove(0);
+    composite.operation();
+
     return 0;
 }
